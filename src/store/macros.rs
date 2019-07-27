b81fb381bc9eaf1ed5bde879a334d7aa44889f6e
Valerian Saliou <valerian@valeriansaliou.name>
2019-07-27 13:08:51 +0200

    Use “try_get” for Redis
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/store/macros.rs
+++ src/store/macros.rs
@@ -6,8 +6,12 @@
 
 macro_rules! get_cache_store_client {
     ($pool:expr, $error:expr, $client:ident $code:block) => {
-        match $pool.get() {
-            Ok(mut $client) => $code,
+        // As DNS server is mono-threaded, it is safe to perform a 'try_get' there, which does \
+        //   not wait if no pool is available to serve request answer. This also prevents all \
+        //   threads from being blocked in the event of a Redis failure, and thus allow \
+        //   Constellation to serve DNS answers from its internal cache.
+        match $pool.try_get() {
+            Some(mut $client) => $code,
             _ => Err($error),
         }
     };
